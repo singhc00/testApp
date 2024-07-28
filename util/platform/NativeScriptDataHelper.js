c59069832eb19dfd3590911b2de6c6dfa9888607
ChandanSingh <chandan.singh@sapowernetworks.com.au>
2024-07-28 13:55:00 +0930

    NS Data Helper

--- util/platform/NativeScriptDataHelper.js
+++ util/platform/NativeScriptDataHelper.js
@@ -2,8 +2,19 @@ sap.ui.define([
     
 ], function() {
   return {
-    readRestService() {
-
+    readRestService(options) {
+        return new Promise((resolve, reject) => {
+            const requestOptions = options;
+            const uniqueId = crypto.randomUUID();
+            requestOptions.uniqueId = uniqueId;
+            window.nsWebViewInterface.emit('restService', requestOptions);
+            window.nsWebViewInterface.on('restService', (responseOptions) => {
+                if(responseOptions.uniqueId === uniqueId) {
+                    resolve();
+                }
+            }); 
+        });
+        
     }
   }
 });
